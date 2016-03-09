6cd62111067783f18019c2d277c19142cbf428ee
mrdulin <novaline@qq.com>
2016-03-09 17:13:36 +0800

    server base function ready

--- server/routes/order.js
+++ server/routes/order.js
@@ -24,5 +24,28 @@ router.get('/:orderNo',(req,res)=>{
 	})
 })
 
+//sample time input 2015-1-1 
+router.get('/checkin/:startdate/:enddate',(req,res)=>{
+	var query = pool.query('select roomNo,RoomName,hotelNo,hotelName,checkintime,checkouttime,customer,orderNo,rentalType,rentalPerDay,telno,channelName,rentMoney,statusName,orderTypeName,handwork_desc from to_check_in_list where checkInDate <= ? and checkInDate>= ?',[req.params.enddate,req.params.startdate],(err,result)=>{
+		console.log(query);
+		if(err)
+		{
+			console.error(err);
+			return;
+		}
+		else
+		{
+			if(result.length == 0 )
+			{
+				res.json("no checkins");
+			}
+			else
+			{
+				res.json(result);
+			}
+		}
+	})
+})
+
 exports = module.exports = router;
 
