3484f0320f1d2f3c88314b98e9f5ac47672cbdcd
mrdulin <novaline@qq.com>
2016-04-18 09:52:01 +0800

    add custom token support - Tokens

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.static('../www/dist'));
 
 app.all('*',function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , Tokens');
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
   if (req.method == 'OPTIONS') {
